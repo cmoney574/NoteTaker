395376497552749a130a5bc0aebdea54d16238c5
Charlie Money <cmoney5748@gmail.com>
2022-02-12 18:07:28 -0500

    added some starter code as well as got the server to run

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const routes = require('./routes/htmlroutes')
+const apiroutes = require('./routes/apiroutes')
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiroutes);
+app.use('/', routes);
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
