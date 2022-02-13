270c9cbae146e3965cde6ae1a4753308069ed07c
Charlie Money <cmoney5748@gmail.com>
2022-02-13 14:12:10 -0500

    got html to display through routes

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const routes = require('./routes/htmlroutes')
 const apiroutes = require('./routes/apiroutes')
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
 app.use('/api', apiroutes);
 app.use('/', routes);
 
