53ea4daad51c4b5f392b351ae07de7f9b7bc77f2
Charlie Money <cmoney5748@gmail.com>
2022-02-13 17:17:58 -0500

    added ability to save a new note

--- db/save.js
+++ db/save.js
@@ -1,8 +1,7 @@
 const util = require('util');
 const fs = require('fs');
 
-const { v4: uuidv4 } = require('uuid');
-uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+const uuidv1 = require('uuid/v1');
 
 const fileRead = util.promisify(fs.readFile);
 const fileWrite = util.promisify(fs.writeFile);
@@ -15,24 +14,27 @@ class Save {
         return fileWrite('db/db.json', JSON.stringify(note))
     }
 
-    getNotes() {
-        return this.readjson().then((notes) => {
-          let savedNotes = [].concat(JSON.parse(notes))
-    
-          return savedNotes;
-        });
+    async getNotes() {
+        const notes = await this.readjson();
+        let savedNotes = [].concat(JSON.parse(notes));
+        return savedNotes;
       }
-    addNote(){
+    async addNote(note){
         const { title, text } = note;
         if (!title || !text) {
             throw new Error("Title or text is blank");
           }
-        const newNote = { title, text, id: uuidv4() };
-        return this.getNotes()
-        .then((notes) => [...notes, newNote])
-        .then((updatedNotes) => this.write(updatedNotes))
-        .then(() => newNote);
+        const newNote = { title, text, id: uuidv1() };
+        const notes = await this.getNotes();
+        const updatedNotes = [...notes, newNote];
+        this.writejson(updatedNotes);
+        return newNote;
     }
+    async deleteNote(id) {
+        const notes = await this.getNotes();
+        const filteredNotes = notes.filter((note) => note.id !== id);
+        return this.write(filteredNotes);
+      }
 }
 
 module.exports = new Save();
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,3 +1,15 @@
 const router = require('express').Router();
+const savedNotes = require('../db/save');
+
+router.get('/notes', (req, res) => {
+    savedNotes.getNotes().then((notes) => {
+        return res.json(notes)
+    })
+})
+
+router.post('/notes', (req, res) => {
+    savedNotes.addNote(req.body)
+    .then((note) => res.json(note))
+})
 
 module.exports = router;
